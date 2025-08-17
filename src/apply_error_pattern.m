1b09482ce6bd07f18607fc5a1292b0348ae7d2b0
Sid Richards <siddrichards@hotmail.co.uk>
2025-08-17 11:47:53 +0100

    Update apply_error_pattern.m

--- src/apply_error_pattern.m
+++ src/apply_error_pattern.m
@@ -1,3 +1,6 @@
+% Deterministically apply a chosen error pattern [e1 e2 e3].
+% Each entry ei=1 applies X on that qubit.
+
 function psi_out = apply_error_pattern(psi, e)
 % e is [e1 e2 e3] with entries 0 or 1
 [~,X,~,~] = pauli();
