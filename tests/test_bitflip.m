68eddc187914f694333cf8135bfbb2ebf2a5f237
Sid Richards <siddrichards@hotmail.co.uk>
2025-09-09 22:13:04 +0000

    comment doublespace

--- tests/test_bitflip.m
+++ tests/test_bitflip.m
@@ -5,7 +5,7 @@ tests = functiontests(localfunctions);
 end
 
 function testEncodeDecodeNoNoise(~)
-psi = encode_bitflip([0.6; 0.8]); % unnormalized OK here
+psi = encode_bitflip([0.6; 0.8]);  % unnormalized OK here
 s = syndrome_bitflip(psi);
 psi2 = correct_bitflip(psi,s);
 b = decode_majority(psi2);
